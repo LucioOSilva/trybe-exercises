d4123399b56134796ab0ba42953385ade4c931be
LucioOSilva <lucioosilva00@gmail.com>
2021-01-27 19:17:34 -0300

    exercicio3 mars sixty percent accuracy to send added

--- exercises/bloco_9/exercicio3.js
+++ exercises/bloco_9/exercicio3.js
@@ -10,8 +10,6 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-
-
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
@@ -22,9 +20,6 @@ function sendMarsTemperature(callback) {
     callback(getMarsTemperature());
   }, messageDelay());
 }
-
-//sendMarsTemperature(getMarsTemperature());
-
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
